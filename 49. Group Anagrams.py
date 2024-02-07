2e4246009b9a84ccd6ba7383e0993bb8ad3fafad
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-02-06 23:20:38 -0600

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,1 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = collections.defaultdict(list)
+        for ch in strs:
+            
+            res[tuple(sorted(ch))].append(ch)
+            # print(res.values())
+        return res.values()
 
+
+
+
+        # res = collections.defaultdict(list)
+
+        # for s in strs:
+        #     count = [0]*26
+        #     for c in s:
+        #         count[ord(c)-ord('a')]+=1
+        #     res[tuple(count)].append(s)
+        # return res.values()
