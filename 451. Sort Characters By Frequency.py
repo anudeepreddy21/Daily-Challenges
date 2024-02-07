84df80b141e56ce81faa8b409cedd7d2368dd973
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-02-07 13:45:52 -0600

    Update 451. Sort Characters By Frequency.py

--- 451. Sort Characters By Frequency.py
+++ 451. Sort Characters By Frequency.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def frequencySort(self, s: str) -> str:
+      res = collections.Counter(s)
+      
+      for letter, freq in res.most_common():
+        res.append(letter * freq)
+      return "".join(res)
