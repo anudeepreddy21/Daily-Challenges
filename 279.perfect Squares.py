4882569fe9304c06954bf65ea196e39bb8279b63
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-02-08 14:02:05 -0600

    Update 279.perfect Squares.py

--- 279.perfect Squares.py
+++ 279.perfect Squares.py
@@ -1,1 +1,23 @@
 
+
+# TLE :
+        # if n == 0:
+        #     return 0
+        # elif n == 1:
+        #     return 1
+        # squres = [i*i for i in range(1, n//2 + 1) if i * i <= n]
+        # ans = float('inf')
+        
+        # def helper(remaining, idx, count):
+        #     nonlocal ans
+        #     if remaining == 0:
+        #         ans = min(ans, count)
+        #         return
+        #     if remaining < 0:
+        #         return
+        #     for i in range(idx, len(squres)):
+        #         count += 1
+        #         helper(remaining - squres[i], idx, count)
+        #         count -= 1
+        # helper(n, 0, 0)
+        # return ans
