3ce6868b1a9e1701da2b29555755919b6ea472f2
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-02-14 15:44:52 -0600

    Update 2149. Rearrange Array Elements by Sign.py

--- 2149. Rearrange Array Elements by Sign.py
+++ 2149. Rearrange Array Elements by Sign.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        if length % 2 != 0:
+            return []
+        # eve_idx, odd_idx = 0, 1
+        pos_idx, neg_idx, ans = 0, 1, [0] * length
+        for num in nums:
+            if num >= 0:
+                ans[pos_idx] = num
+                pos_idx +=2
+            else:
+                ans[neg_idx] = num
+                neg_idx += 2
+        return ans
