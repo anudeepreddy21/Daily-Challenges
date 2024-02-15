4d84a492fb94da21bc57c2fcf82376160a1a3cb5
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-02-15 11:34:52 -0600

    Update 2971. Find Polygon With the Largest Perimeter.py

--- 2971. Find Polygon With the Largest Perimeter.py
+++ 2971. Find Polygon With the Largest Perimeter.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+      nums.sort() #sort cheskuntundi.. so that it keeps the track of the elements from smaller lengths to larger lengths
+      prev_sum = 0 # idi previous elements sum maintain chydaniki, so that ee sum ni current element tho compare chesi, thakkuvunte ans la assign ccheskovali ledante move forward
+      ans = -1
+      for num in nums:
+        if num < prev_sum:
+          ans = num + prev_sum
+        prev_sum += num
+      return ans
