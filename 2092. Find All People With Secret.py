f1e87c621e6601e2a3068b376d2baff44b7c5cf7
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-02-24 15:10:09 -0600

    Update 2092. Find All People With Secret.py

--- 2092. Find All People With Secret.py
+++ 2092. Find All People With Secret.py
@@ -1,1 +1,24 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+      secrets = set([0, firstPerson])
+      time_map = {}
 
+      for src, des, t in meetings:
+        if t not in time_map:
+          time_map[t] = defaultdict(list)
+        time_map[t][src].append(des)
+        time_map[t][des].append(src)
+
+      def dfs(src, adj):
+        if src in visit:
+          return
+        visit.add(src)
+        secrets.add(src)
+        for nei in adj[src]:
+          dfs(nei, adj[src])
+      for t in sorted(time_map.keys()):
+        visit = set()
+        for src in time_map[t]:
+          dfs(src,time_map[t])
+
+  return list(secrets)
