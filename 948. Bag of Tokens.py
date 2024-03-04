33548512f83230421bdc9da62db4a562b67574b1
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-03-04 12:56:31 -0600

    Update 948. Bag of Tokens.py

--- 948. Bag of Tokens.py
+++ 948. Bag of Tokens.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+      tokens.sort()
+      score = max_score = 0
+      left, right = 0, len(tokens) - 1
+      while left <= right:
+        if power > tokens[i]:
+          power -= tokens[i]
+          left += 1
+          score += 1
+          max_score = max(score, max_score)
+        elif score and right > left + 1:
+          power += tokens[i]
+          right -= 1
+          score -= 1
+        else:
+          break
+      return max_score
+          
