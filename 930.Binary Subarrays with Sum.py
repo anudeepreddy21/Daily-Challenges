5392f77c42bc1d2bd37d99204fca32b0f274bc30
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-03-14 12:14:35 -0500

    Update 930.Binary Subarrays with Sum.py

--- 930.Binary Subarrays with Sum.py
+++ 930.Binary Subarrays with Sum.py
@@ -13,3 +13,10 @@ class Solution:
                 res += (r - l + 1)
             return res
         return helper(goal) - helper(goal - 1)
+
+# Ikkada ee problem chudagane intuition vachesi,, 2 pointer sliding window laga vadi subarrays sum chustoo count chydam subarrays ni. but ila chesthe O(n**2) ** padtundi.
+# so clever way of solving thios problem is... Nenu  okate loop laga vadi first I will find out the result for <= goal. Deeni valla manaki exact goal ki kavalsina subarrays summation count radu.. but kindavi anni vastayi.
+# => So manaki vahina ipudu <= goal subarray count , manaki kavalsindi == goal subarray count
+# => idi solve chydaniki nenu malli <= goal - 1 subarray count techkunta.
+# => Now we have subarray count for <= goal and sb. count for <= goal -1.
+# => Count (res == goal) can be scieved by. doing --> count(<= goal) - Count( <= goal - 1)
