d29bae031b4d3ba57684b86bfc16db1d56c85e0f
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-03-14 12:13:51 -0500

    Update 930.Binary Subarrays with Sum.py

--- 930.Binary Subarrays with Sum.py
+++ 930.Binary Subarrays with Sum.py
@@ -1,1 +1,15 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
 
+        def helper(x):
+            if x < 0: return 0 # if goal is passed as zero, function call will be helper(-1), to avoid this
+            res = 0 # To keep count of subarrays
+            l, cur_sum = 0, 0
+            for r in range(len(nums)):
+                cur_sum += nums[r]
+                while cur_sum > x:
+                    cur_sum -= nums[l]
+                    l += 1
+                res += (r - l + 1)
+            return res
+        return helper(goal) - helper(goal - 1)
