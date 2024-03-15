c8efd5a7fa84b3f396bee7df40950de96f4be94b
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-03-15 10:32:01 -0500

    Update 238.Product of Array Except Itself.py

--- 238.Product of Array Except Itself.py
+++ 238.Product of Array Except Itself.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+      length = len(nums)
+      ans = [1] * length
+      for L in range(length):
+        ans[L] = ans[L - 1] * nums[L - 1]
+      R = 1
+      for i in range(length - 1, -1, -1):
+        ans[i] = ans[i] * R
+        R = R * nums[i]
+      retuyrn ans
