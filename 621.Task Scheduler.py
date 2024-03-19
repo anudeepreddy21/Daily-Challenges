dc1d30057167a13b7dfdd778c5af582841324679
Anudeep Guntaka <102184193+anudeepreddy21@users.noreply.github.com>
2024-03-19 10:22:46 -0500

    Update 621.Task Scheduler.py

--- 621.Task Scheduler.py
+++ 621.Task Scheduler.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+      time, freq = 0, Counter(tasks)
+      maxHeap = [- cnt for cnt in freq.values()]
+      que = dequeue()
+      while que or maxHeap:
+        time += 1
+        if maxHeap:
+          cnt = heapq.heappop(maxHeap) + 1
+          if cnt:
+            que.append([cnt, time + n])
+        if que and que[0][1] == time:
+          heapq.heappush(maxHeap, que.popleft()[0])
+      return time
